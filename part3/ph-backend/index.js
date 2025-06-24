8200afb71aa0848871e21256d08241f9bb9da5e4
guruprasath3200Burner <anon295123@gmail.com>
2025-06-24 09:32:05 +0800

    uibuild

--- part3/ph-backend/index.js
+++ part3/ph-backend/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-
 const app = express();
+
+app.use(express.static("dist"));
 app.use(cors());
 app.use(express.json());
 morgan.token("body", function (req, res) {
