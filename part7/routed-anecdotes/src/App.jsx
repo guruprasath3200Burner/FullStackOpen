840a73fb2921ca5aaa9a4942bb7d20dddef34dfd
guruprasath3200Burner <anon295123@gmail.com>
2025-07-01 16:03:56 +0800

    FInished 7.4 -7.6

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
+import { useField } from "./hooks/index";
 import {
   BrowserRouter as Router,
   Routes,
@@ -61,18 +62,25 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  const { reset: resetContent, ...content } = useField("text");
+  const { reset: resetAuthor, ...author } = useField("text");
+  const { reset: resetInfo, ...info } = useField("text");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
+  };
+
+  const handleClear = (e) => {
+    e.preventDefault();
+    resetContent();
+    resetAuthor();
+    resetInfo();
   };
 
   return (
@@ -81,29 +89,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} placeholder="https://example.com" />
         </div>
         <button>create</button>
+        <button onClick={handleClear}>clear</button>
       </form>
     </div>
   );
@@ -201,7 +198,7 @@ const Anecdote = ({ anecdote }) => {
   );
 };
 Anecdote.propTypes = {
-  anecdote: PropTypes.object,
+  anecdote: PropTypes.array.isRequired,
 };
 
 export default App;
